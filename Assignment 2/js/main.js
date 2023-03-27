91a15bb52e730803ce45264d7ad3898c1b335586
Ruslan <ru.abdullin@innopolis.university>
2023-03-27 14:20:11 +0300

    Initial commit

---
+++ Assignment 2/js/main.js
@@ -0,0 +1,16 @@
+/*global $, jQuery, alert*/
+$(document).ready(function() {
+
+  const typed = $(".typed");
+
+  $(function() {
+    typed.typed({
+      strings: ["Ruslan Abdullin.", "Robotics Engineer.", "Java Programmer.", "Developer.", "Anime Nerd."],
+      typeSpeed: 80,
+      loop: true,
+    });
+  });
+
+  getISS();
+
+});
